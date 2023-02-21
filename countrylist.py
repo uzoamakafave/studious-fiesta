6355d97501a5a2fd774af2a4871974e8f5cd813a
Uzoamaka <favouritechuks.fc@gmail.com>
2023-02-21 14:16:20 +0100

    update

--- countrylist.py
+++ countrylist.py
@@ -7,7 +7,7 @@ def index():
     countries = ["Nigeria","Tunisia","UK","Germany"]
     return render_template('countrylist.html', countries = countries)
 
-if __name__ == '__main__  ' :
+if __name__=='__main__':
     app.run(host="0.0.0.0", port=5001)
 
     
